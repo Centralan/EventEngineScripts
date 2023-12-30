04201a08dc9366fe92385dc58c113db7dfe58237
Centralan <Jessegauthier070296@gmail.com>
2023-12-29 21:43:04 -0500

    Delete scripts directory

--- scripts/survival3.lua
+++
@@ -1,9 +0,0 @@
-local myWorld = World:new('survival3');
-
-function lobby_easter_room(data)
-         function lobby_easter_room(data)
-          p:sendEvent("achievement.timecapsule");
-     end
-end
-
-registerHook("REGION_ENTER", "lobby_easter_room", "survival3-spawn_easter");
