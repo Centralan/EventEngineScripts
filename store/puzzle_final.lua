25dbe357e8c4ba9f1862253cc8cc28b29120cd08
Centralan <Jessegauthier070296@gmail.com>
2023-12-29 21:43:10 -0500

    Delete store directory

--- store/puzzle_final.lua
+++
@@ -1,7 +0,0 @@
-function puzzleFinal(data)
-	local playerName = data["player"];
-	local kru = Player:new("Kruithne");
-	kru:sendMessage("&c" .. playerName .. " has entered the final stretch");
-end
-
---registerHook("REGION_ENTER", "puzzleFinal", "puzzle-puzzfinal");
